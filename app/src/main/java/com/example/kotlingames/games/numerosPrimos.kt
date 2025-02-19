b564ed1c784a37845a6d33aa891dc76b2e8c764e
Greys2004 <gav77465@lasallebajio.edu.mx>
2025-02-18 21:33:04 -0600

    Cambios a NumerosPrimos

--- app/src/main/java/com/example/kotlingames/games/numerosPrimos.kt
+++ app/src/main/java/com/example/kotlingames/games/numerosPrimos.kt
@@ -1,8 +1,34 @@
 package com.example.kotlingames.games
 
 fun main(){
-    println("Por favor ingresa un numero")
-    var numberInput = readlnOrNull()?.toIntOrNull()
+    var esPrimo = false
 
+    while(esPrimo == false) {
+        println("Por favor ingresa un numero")
+        var numberInput = readlnOrNull()?.toIntOrNull()
 
+        if (numberInput != null) {
+            if (numberInput <= 1) {
+                esPrimo = false
+            } else {
+                esPrimo = true
+                var i = 2
+                while (i <= numberInput / 2) {
+                    if (numberInput % i == 0) {
+                        esPrimo = false
+                        break
+                    }
+                    i++
+                }
+            }
+        } else {
+            println("Ingrese un numero valido")
+        }
+
+        if (esPrimo) {
+            println("El numero $numberInput es primo")
+        } else {
+            println("El numero $numberInput no es primo")
+        }
+    }
 }
